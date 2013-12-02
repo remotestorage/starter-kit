76c26155c394e907cf4570975698521211b10eb2
Michiel@unhosted <michiel@unhosted.org>
2013-12-02 22:41:23 +0200

    split further into localhost-server and storage server per se

--- remotestorage-server.js
+++ remotestorage-server.js
@@ -2,7 +2,7 @@ var fs = require('fs'),
   url = require('url'),
   crypto = require('crypto');
 
-exports.createInstance = function(kv, config) {
+exports.createServer = function(kv) {
   function makeScopePaths(userName, scopes) {
     var scopePaths=[];
     for(var i=0; i<scopes.length; i++) {
@@ -21,33 +21,6 @@ exports.createInstance = function(kv, config) {
     console.log(str);
   }
   
-  function addToken(token, scopes) {
-    kv.set('tokens:'+token, makeScopePaths('me', scopes));
-  }
-
-  function createInitialTokens() {
-    if(! config.initialTokens) {
-      return;
-    }
-    for(var token in config.initialTokens) {
-      var scopePaths = makeScopePaths(
-        config.defaultUserName, config.initialTokens[token]
-      );
-      log('adding ',scopePaths,' for', token);
-      kv.set('token:'+token, scopePaths);
-    }
-  }
-
-  function createToken(userName, scopes, cb) {
-    crypto.randomBytes(48, function(ex, buf) {
-      var token = buf.toString('hex');
-      var scopePaths = makeScopePaths(userName, scopes);
-      log('createToken ',userName,scopes);
-      log('adding ',scopePaths,' for',token);
-      kv.set('tokens:'+token, scopePaths);
-      cb(token);
-    });
-  }
   function mayRead(authorizationHeader, path) {
     if(authorizationHeader) {
       var scopes = kv.get('tokens:'+authorizationHeader.substring('Bearer '.length));
@@ -112,13 +85,6 @@ exports.createInstance = function(kv, config) {
   function writeJson(res, obj, origin, timestamp) {
     writeRaw(res, 'application/json', JSON.stringify(obj), origin, timestamp);
   }
-  function writeHtml(res, html) {
-    res.writeHead(200, {
-      'content-type': 'text/html'
-    });
-    res.write('<!DOCTYPE html lang="en"><head><title>'+config.host+'</title><meta charset="utf-8"></head><body>'+html+'</body></html>');
-    res.end();
-  }
   function give404(res, origin) {
     log('404');
     writeHead(res, 404, origin);
@@ -128,67 +94,6 @@ exports.createInstance = function(kv, config) {
     log('COMPUTER_SAYS_NO - '+status);
     writeHead(res, status, origin, timestamp);
     res.end();
-  }
-  function toHtml(str) {
-    return str
-      .replace(/&/g, '&amp;')
-      .replace(/</g, '&lt;')
-      .replace(/>/g, '&gt;')
-      .replace(/"/g, '&quot;');
-  }
-  function portal(req, res) {
-    var urlObj = url.parse(req.url, true);
-    res.writeHead(200, {
-      'content-type': 'text/html'
-    });
-    res.write('<!DOCTYPE html lang="en"><head><title>'+config.host+'</title><meta charset="utf-8"></head><body><ul>');
-    var outstanding = 0;
-    for(var i in config.apps) {
-      outstanding++;
-      (function(i) {
-        createToken(config.defaultUserName, [':rw'], function(token) {
-          res.write('<li><a href="'+i+'#remotestorage=me@localhost'
-                    +'&access_token='+token+'">'+config.apps[i]+'</a></li>');
-          outstanding--;
-          if(outstanding==0) {
-            res.write('</ul></body></html>');
-            res.end();
-          }
-        });
-      })(i);
-    }
-  }
-  function webfinger(req, res) {
-    var urlObj = url.parse(req.url, true);
-    log('WEBFINGER');
-    if(urlObj.query['resource']) {
-      userAddress = urlObj.query['resource'].substring('acct:'.length);
-      userName = userAddress.split('@')[0];
-    }
-    writeJson(res, {
-      links:[{
-        href: 'http://'+config.host+':'+config.port+'/storage/'+userName,
-        rel: "remotestorage",
-        properties: {
-          'http://remotestorage.io/spec/version': 'draft-dejong-remotestorage-02',
-          'http://tools.ietf.org/html/rfc6749#section-4.2': 'http://'+config.host+':'+config.port+'/auth/'+userName,
-          'http://tools.ietf.org/html/rfc6750#section-2.3': false,
-          'http://tools.ietf.org/html/rfc2616#section-14.16': false
-        }
-      }]
-    });
-  }
-  function oauth(req, res) {
-    var urlObj = url.parse(req.url, true);
-    var scopes = decodeURIComponent(urlObj.query['scope']).split(' '),
-    clientId = decodeURIComponent(urlObj.query['client_id']),
-    redirectUri = decodeURIComponent(urlObj.query['redirect_uri']),
-    clientIdToMatch,
-    userName;
-    var userName = urlObj.pathname.substring('/auth/'.length);
-    createToken(userName, scopes, function(token) {
-      writeHtml(res, '<a href="'+toHtml(redirectUri)+'#access_token='+toHtml(token)+'">Allow</a>');
-    });
   }
   function getVersion(path) {
     if(kv.get('version:'+path)) {
@@ -225,7 +130,6 @@ exports.createInstance = function(kv, config) {
       items: items
     };
   }
-      
   function storage(req, res) {
     var urlObj =  url.parse(req.url, true);
     var path=urlObj.pathname.substring('/storage/'.length);
@@ -363,10 +267,5 @@ exports.createInstance = function(kv, config) {
     }
   }
 
-  return {
-    portal: portal,
-    webfinger: webfinger,
-    oauth: oauth,
-    storage: storage
-  };
+  return storage;
 };
